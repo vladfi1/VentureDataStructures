6595cfe1ff0b1194da4f6558e6630fed9b7c0a85
vladfi1 <vladfi2@gmail.com>
2014-05-01 20:01:05 -0400

    Simple setup script.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup (
+    name = 'Venture Library',
+    version = '0.1',
+    author = 'Vlad Firoiu',
+    packages = ['venturelib'],
+    package_dir={"venturelib":"."}
+)
